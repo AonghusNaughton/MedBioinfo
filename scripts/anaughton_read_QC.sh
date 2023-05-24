36c832fc34eb44b9bd6f95818ff92548323c7d8e
Aonghus Naughton <aonghus.naughton@ki.se>
2023-05-24 16:27:30 +0200

    QC script update

--- scripts/anaughton_read_QC.sh
+++ scripts/anaughton_read_QC.sh
@@ -9,51 +9,81 @@
 #SBATCH --mail-type=FAIL
 
 echo "script start: download and initial sequencing read quality control"
-date 
-
-sqlite3 -batch -csv -noheader /shared/projects/2314_medbioinfo/pascal/central_database/sample_collab.db "select run_accession from sample_annot spl left join sample2bioinformatician s2b using(patient_code) where username='anaughton';" > ./anaughton_run_accessions.txt
+date
 
 module load sra-tools
+module load fastq
+module load flash2
+module load bowtie2
 
-cat anaughton_run_accessions.txt | srun --cpus-per-task=1 --time=00:30:00 xargs fastq-dump --readids --gzip --outdir ../data/sra_fastq/input --split-3
 
-# To count number of reads in the downloaded fastq files:
-for i in `ls ../data/sra_fastq/input/*.fastq.gz`; do echo -n "$i: " ; echo $(zcat ${i} | wc -l)/4|bc; done
+sqlite3 -batch -csv -noheader /shared/projects/2314_medbioinfo/pascal/central_database/sample_collab.db \
+"select run_accession
+from sample_annot
+spl left join sample2bioinformatician
+s2b using(patient_code) where username='anaughton';" > ./anaughton_run_accessions.txt
 
-# To get fastq stats 
-srun --time=00:10:00 --cpus-per-task=4 seqkit stats -a -T ../data/sra_fastq/input/*.fastq.gz > ../data/fastq_stats/sra_fastq_stats.txt
+mkdir ../data/sra_fastq
 
-awk '{print$1" "$4" "$5}' ../data/fastq_stats/sra_fastq_stats.txt > ../data/fastq_stats/sra_fastq_stats_modified.txt
+cat anaughton_run_accessions.txt | srun --cpus-per-task=1 --time=00:30:00 \
+xargs fastq-dump --readids --gzip --outdir ../data/sra_fastq --split-3 --disable-multithreading
 
-# Create txt file with number of reads and bases from values in meta data 
+mkdir ../data/fastq_stats
+
+# To get fastq stats
+srun --time=00:10:00 --cpus-per-task=2 seqkit stats --threads 2 -a -T ../data/sra_fastq/*.fastq.gz > ../data/fastq_stats/sra_fastq_stats.txt
+
+# Create txt file with number of reads and bases from values in meta data
 srun --time=00:10:00 --cpus-per-task=1 \
 sqlite3 -csv -header \
 /shared/projects/2314_medbioinfo/pascal/central_database/sample_collab.db \
 "select run_accession, total_reads, total_reads / 2 as total_reads_split,
-base_count, base_count / 2 as base_count_split  
-from sample_annot 
-outer left join sample2bioinformatician on 
+base_count, base_count / 2 as base_count_split
+from sample_annot
+outer left join sample2bioinformatician on
 sample_annot.patient_code = sample2bioinformatician.patient_code
 where username = 'anaughton'" > ../data/fastq_stats/metadata_stats.txt
 
-# Removes duplicated sequences and writes to new file + 
-# lists them in seperate file 
-cd ~/aonghus_medbioinfo/MedBioinfo/data/sra_fastq/input
-for f in *.gz
-do
-	filename="${f%.*.*}"
-	srun --time=00:30:00 --cpus-per-task=4 \
-	zcat ${f} | seqkit rmdup -s -i -o ../output/${filename}.clean.fq.gz \
-	-d ../output/${filename}.duplicated.fq.gz -D ../output/${filename}.duplicated.detail.txt
-done
+# Removes duplicated sequences and writes to new file +
+# lists them in seperate file
 
-cd ~/aonghus_medbioinfo/MedBioinfo/analyses
-echo -e "\nChecking for full adapter sequences: \n"
-../scripts/check_adapters.sh AGATCGGAAGAGCACACGTCTGAACTCCAGTCA AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT
-echo -e "\nChecking for shortened adapter sequences: \n"
-../scripts/check_adapters.sh AGATCGGAAGAGCACACGTCTGA AGATCGGAAGAGCGTCGTGTAGG
+mkdir ../data/rmdup_output
+
+srun --cpus-per-task=2 --time=00:10:00 xargs -a anaughton_run_accessions.txt -I{} \
+zcat ../data/sra_fastq/{}_1.fastq.gz | seqkit rmdup -s -i -o ../data/rmdup_output/{}_1.clean.fq.gz \
+-d ../data/rmdup/{}_1.duplicated.fq.gz -D ../data/rmdup/{}_1.duplicated.detail.txt
+
+srun --cpus-per-task=2 --time=00:10:00 xargs -a anaughton_run_accessions.txt -I{} \
+zcat ../data/sra_fastq/{}_2.fastq.gz | seqkit rmdup -s -i -o ../data/rmdup_output/{}_2.clean.fq.gz \
+-d ../data/rmdup/{}_2.duplicated.fq.gz -D ../data/rmdup/{}_2.duplicated.detail.txt
+
+# fastqc
+mkdir ./fastqc
+srun --cpus-per-task=2 --time=00:30:00 xargs -I{} -a anaughton_run_accessions.txt \
+fastqc --outdir ./fastqc --threads 2 --noextract ../data/sra_fastq/{}_1.fastq.gz ../data/sra_fastq/{}_2.fastq.gz
+
+# Merge paired-ends with flash2
+mkdir ../data/merged_pairs
+srun --cpus-per-task=2 --time=00:30:00 xargs -a anaughton_run_accessions.txt -n 1 -I{} flash2 --threads=2 -z \
+--output-directory ../data/merged_pairs/ --output-prefix {}.flash --max-overlap 120 ../data/sra_fastq/{}_1/fastq.gz \
+../data/sra_fastq/{}_2.fastq.gz 2>&1 | tee -a anaughton_flash2.log
+
+mkdir ../data/reference_seqs
+efetch -db nuccore -id NC_001422 -format fasta > ../data/reference_seqs/PhiX_NC_001422.fna
+efetch -db nuccore -id NC_045512 -format fasta > ../data/reference_seqs/SARCoV2_NC_045512.fna
+
+mkdir ../data/bowtie2_DBs
+srun bowtie2-build -f ../data/reference_seqs/PhiX_NC_001422.fna ../data/bowtie2_DBs/PhiX_bowtie2_DB
+srun bowtie-build -f ../data/reference_seqs/SARCoV_2_NC_045512.fna ../data/bowtie2_DBs/SARCoV2_bowtie2_DB
+
+mkdir bowtie
+srun --cpus-per-task=8 bowtie2 -x ../data/bowtie2_DBs/PhiX_bowtie2_DB -U ../data/merged_pairs/ERR*.extendedFrags.fastq.gz \
+-S bowtie/anaughton_merged2PhiX.sam --threads 8 --no-unal 2>&1 | tee bowtie/anaughton_bowtie_merged2PhiX.log
+
+srun --cpus-per-task=8 bowtie2 -x ../data/bowtie2_DBs/SARCoV2_bowtie2_DB -U ../data/merged_pairs/ERR*.extendedFrags.fasta.gz \
+-S bowtie/anaughton_merged2SARCoV2.sam --threads 8 --no-unal 2>&1 | tee bowtie/anaughton_bowtie_merged2SARCoV2.log
 
 
-	
+
 date
 echo "script end."
